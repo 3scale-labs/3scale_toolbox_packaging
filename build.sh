05cb351e7d5af67765b1198ce75fc2d0366a91e2
Eguzki Astiz Lezaun <eastizle@redhat.com>
2021-08-25 14:15:14 +0200

    migrate circleci

--- build.sh
+++ build.sh
@@ -3,17 +3,6 @@ set -o errexit
 set -o nounset
 set -o xtrace
 
-DOCKER_IMAGE="${DOCKER_IMAGE:-}"
-
-if [ "${DOCKER_IMAGE}" = "centos/ruby-27-centos7" ]
-then
-    yum -y install rpm-build yajl yajl-devel
-elif [ "${DOCKER_IMAGE}" = "ubuntu:20.04" ]
-then
-    apt update
-    apt install -y git ruby ruby-dev libffi-dev build-essential
-fi
-
 # Since secure_path is set on our CentOS build images, we also need to ensure
 # we only use absolute paths when referencing the binaries we want to run.
 GEM="$(command -v gem)"
--- docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -o errexit
-set -o nounset
-set -o xtrace
-
-BUILD_AT="/build"
-
-docker run -u root -e DOCKER_IMAGE=${DOCKER_IMAGE} -it --rm -v "${PWD}:${BUILD_AT}" -w "${BUILD_AT}" "$DOCKER_IMAGE" ./native.sh
