344c1df52d60ee35809ecaa0b02a9b043e1d64ee
Eguzki Astiz Lezaun <eastizle@redhat.com>
2021-08-25 13:30:12 +0200

    upgrade bundler v2.2.21

--- native.sh
+++ native.sh
@@ -7,17 +7,17 @@ DOCKER_IMAGE="${DOCKER_IMAGE:-}"
 
 if [ "${DOCKER_IMAGE}" = "centos/ruby-27-centos7" ]
 then
-    yum -y install rpm-build
+    yum -y install rpm-build yajl yajl-devel
 elif [ "${DOCKER_IMAGE}" = "ubuntu:20.04" ]
 then
     apt update
     apt install -y git ruby ruby-dev libffi-dev build-essential
-    gem install bundler:2.1.4
 fi
 
 # Since secure_path is set on our CentOS build images, we also need to ensure
 # we only use absolute paths when referencing the binaries we want to run.
 GEM="$(command -v gem)"
+$GEM install bundler:2.2.21
 BUNDLE="$(command -v bundle)"
 RUBY="$(command -v ruby)"
 
